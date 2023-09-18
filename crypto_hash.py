ad97edd5f01526afa9b06b1f823b38ed4a00eda2
Riki <iceishere00@gmail.com>
2023-09-19 01:20:11 +0330

    blockchain application

---
+++ crypto_hash.py
@@ -0,0 +1,26 @@
+import hashlib
+import json
+
+def stringfy(data):
+    return json.dumps(data)
+
+
+def crypto_hash(*args):
+    """
+    Return a sha-251 hash of the given arguments.
+    """
+
+    stringfied_args = map(stringfy, args)
+    print(f'stringfied_args: {stringfied_args}')
+
+    joined_data = ''.join(stringfied_args)
+    print(f'joined_data: {joined_data}')
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
+
+
+def main():
+    print(f"crypto_hash('1', 'two', 3): {crypto_hash('1', 'two', 3)}")
+
+
+if __name__ == '__main__':
+    main()
